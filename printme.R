9ca77f04946eb64c4e492eb040950e0dfaac8eee
Cornelia <cornelia.gru@gmail.com>
2018-06-27 14:58:18 +0200

    added wrapper function

--- printme.R
+++ printme.R
@@ -1,4 +1,8 @@
-name = "<your name>"
-eyecolor = "<your eyevcolor>"
-meal = "<your favorite meal>"
-print(paste0(name, " has ", eyecolor, " eyes and enjoys eating ", meal, " the most!"))
+name = "Cornelia"
+eyecolor = "brown"
+meal = "Lasagne"
+printer <- function(name, eyecolor, meal) {
+  print(paste0(name, " has ", eyecolor, " eyes and enjoys eating ", meal, " the most!"))  
+}
+
+printer(name, eyecolor, meal)
